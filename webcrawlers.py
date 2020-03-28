b3370e7cc0042b77594b3622da0a39189941934f
ewelinawantuch <61844659+ewelinawantuch@users.noreply.github.com>
2020-03-28 21:58:32 +0100

    implemented fetching the whole document

---
+++ webcrawlers.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 21:51:22 2020
+
+@author: Ewelina
+"""
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
