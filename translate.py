6867fedeed07b1e5aa45b25cdfceb016ce2a2a27
ewelinawantuch <61844659+ewelinawantuch@users.noreply.github.com>
2020-04-11 20:09:47 +0200

    Fetching data from api

--- translate.py
+++ translate.py
@@ -7,10 +7,18 @@ Created on Sat Apr 11 19:42:41 2020
 
 
 from googletrans import Translator
+import requests
 
-def main():
+def googletrans():
     translator = Translator()
     result = translator.translate("Jak się masz?", dest='de')
     print(result.text)
     
-main()
+def piratetrans(text):
+    url = 'https://api.funtranslations.com/translate/pirate.json'
+    data = {'text': text}
+    
+    response = requests.post(url, data=data)
+    print(type(response.text))
+    
+piratetrans('Hello,  sir')
